534120153b7bdb09685ec5e19bf34154371ff43d
ReaganBruce <ReaganBruceBirmingham@gmail.com>
2021-03-05 12:19:46 -0600

    updated objects in part 1

--- server/server.js
+++ server/server.js
@@ -1,16 +1,32 @@
 const path = require('path')
 const fs = require('fs');
-let chirpArray = [{
-    "Chirper1": "This chirp number 1!",
-    "Chirper2": "This chirp number 2!",
-    "Chirper3": "This chirp number 3!",
-    "Chirper4": "This chirp number 4!",
-    "Chirper5": "This chirp number 5!"
-}]
+let chirpArray = [
+    {
+        username: "Reagan",
+        message: "yay!!"
+    },
+    {
+        username: "Reagan",
+        message: "yay!!"
+    },
+    {
+        username: "Reagan",
+        message: "yay!!"
+    },
+    {
+        username: "Reagan",
+        message: "yay!!"
+    },
+    {
+        username: "Reagan",
+        message: "yay!!"
+    }
+
+]
 
 let chirpyPath = path.join(__dirname, '../chirps.json');
 
-fs.rename("../chirps.json", JSON.stringify(chirpArray), function (err) {
+fs.writeFile(chirpyPath, JSON.stringify(chirpArray), function (err) {
     if (err) {
         console.log(err)
     }
